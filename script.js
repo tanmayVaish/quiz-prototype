784883aed522a9945cef578fba24f4a44da91161
tanmayVaish <tanmay777leon@gmail.com>
2021-10-21 14:07:09 +0530

    test 4

---
+++ script.js
@@ -0,0 +1,35 @@
+const next = document.querySelector('.button');
+const question1 = document.querySelector('#one');
+const question2 = document.querySelector('#two');
+const submit = document.querySelectorAll('.buttonSubmit');
+const terms = document.querySelector('.terms');
+const overlay = document.querySelectorAll('.overlay');
+
+let prev = null;
+
+next.addEventListener('click', () => {
+  question1.style.display = 'block';
+  terms.style.display = 'none';
+})
+
+submit[0].addEventListener('click', () => {
+  question1.style.display = 'none';
+  question2.style.display = 'block';
+})
+
+
+function timer(){
+  if (prev !== null)
+    prev.style.display = 'none';
+  overlay[i].style.display = 'grid';
+  prev = overlay[i];
+  if(++i<overlay.length){
+    setTimeout(timer, 1000);
+  }
+}
+
+let i=0;
+submit[1].addEventListener('click', () => {
+  question2.classList.add('blur');
+  timer();
+})
